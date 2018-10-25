18025c6861b42919f568f20ae973ac4cc1ff41bd
lakshya <lakshyapal028@gmail.com>
2018-10-25 14:58:42 +0530

    add all

--- js/script.js
+++ js/script.js
@@ -25,10 +25,55 @@ $(document).ready(()=>{
     if(window.innerWidth < 576){
 
     	$('#img-mt').attr('src','assets/logo-small.png')
-        $('.searchdiv').css('display','none')
+      $('.searchdiv').css('display','none')
     	
     }
 
+});
 
+
+var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      freeMode: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+});
+
+// parallax
+/*
+$('.img-holder').imageScroll({
+
+    image: null,
+    imageAttribute: 'image',
+    container: $('body'),
+    speed: 0.2,
+    coverRatio: 0.75,
+    holderClass: 'imageHolder',
+    holderMinHeight: 200,
+    extraHeight: 0,
+    mediaWidth: 1600,
+    mediaHeight: 400,
+    parallax: true,
+    touch: false
 
 });
+
+*/
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
